1174d1d9790e4455f3da395470a59c289a52830d
alex pozhilenkov <alex_pozhilenkov@adoriasoft.com>
2021-06-21 18:27:12 +0300

    update

--- pallets/voting/src/lib.rs
+++ pallets/voting/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::codec::{Decode, Encode};
 pub use pallet::*;
 
 #[cfg(test)]
@@ -10,6 +11,7 @@ mod tests;
 
 #[frame_support::pallet]
 pub mod pallet {
+    use super::*;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
 
@@ -22,13 +24,49 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::error]
-    pub enum Error<T> {}
+    pub enum Error<T> {
+        // emits when from provided VotingSubject has been applied
+        VotingSubjectHasBeenApplied,
+    }
 
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
+    #[pallet::storage]
+    type SomePendingVotings<T: Config> =
+        StorageMap<_, Blake2_128Concat, VotingSubject<T>, T::BlockNumber, OptionQuery>;
+
+    #[pallet::storage]
+    type SomeVotingResults<T: Config> =
+        StorageMap<_, Blake2_128Concat, VotingSubject<T>, VotingResult, OptionQuery>;
+
     #[pallet::call]
     impl<T: Config> Pallet<T> {}
 
-    impl<T: Config> Pallet<T> {}
+    impl<T: Config> Pallet<T> {
+        pub fn create_voting(subject: VotingSubject<T>) -> Result<(), Error<T>> {
+            ensure!(
+                <SomePendingVotings<T>>::get(subject.clone()) == None
+                    && <SomeVotingResults<T>>::get(subject.clone()) == None,
+                <Error<T>>::VotingSubjectHasBeenApplied
+            );
+
+            let block_number = <frame_system::Pallet<T>>::block_number();
+            <SomePendingVotings<T>>::insert(subject, block_number);
+
+            Ok(())
+        }
+    }
+}
+
+#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Encode, Decode, Eq, PartialEq, Debug)]
+pub struct VotingResult {
+    pub result: bool,
+}
+
+#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Encode, Decode, Eq, PartialEq, Debug)]
+pub struct VotingSubject<T: Config> {
+    pub subject_hash: T::Hash,
 }
